0d905135991767505999f9fed8d8953e6ba398b2
dianaplascencia0412 <114034425+dianaplascencia0412@users.noreply.github.com>
2022-10-05 18:53:22 -0500

    Comit agregar doctor a la base de datos

--- app/src/main/java/mx/edu/tecmm/consultorioplanner/AgregarDoctor.kt
+++ app/src/main/java/mx/edu/tecmm/consultorioplanner/AgregarDoctor.kt
@@ -2,10 +2,43 @@ package mx.edu.tecmm.consultorioplanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class AgregarDoctor : AppCompatActivity() {
+    lateinit var txtNombre: EditText
+    lateinit var txtId: EditText
+    lateinit var db :consultorioplanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_doctor)
+        txtId = findViewById(R.id.edId)
+        txtNombre= findViewById(R.id.edNombre)
+
+        db = this.application as consultorioplanner
+        supportActionBar?.hide()
+
+    }
+    fun agregarDoctor(v: View) {
+
+        //Aqui agrego un doctor a la base de datos
+        val id= txtId.text.toString()
+        val nombre = txtNombre.text.toString()
+        if(id.equals("") or nombre.equals("")){
+            Toast.makeText(this, "Error complete todos los datos", Toast.LENGTH_LONG).show()
+        }
+        else {
+            lifecycleScope.launch {
+                val doctor = Doctores(id, nombre)
+                db.room.DoctoresDao().insert(doctor)
+            }
+            Toast.makeText(this, "Doctor Registrado", Toast.LENGTH_LONG).show()
+            finish()
+
+        }
     }
 }
--- app/src/main/java/mx/edu/tecmm/consultorioplanner/MainActivity.kt
+++ app/src/main/java/mx/edu/tecmm/consultorioplanner/MainActivity.kt
@@ -4,44 +4,59 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.launch
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
    lateinit var recycler: RecyclerView
+    lateinit var db :consultorioplanner
 
-    companion object{
-        val doctores =  ArrayList<Doctores> ()
-    }
+   /* companion object{
+        //val doctores =  ArrayList<Doctores> ()
+    }*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycler = findViewById(R.id.rv_persona)
-        doctores.add(Doctores("123456" , "Diana Rios"))
-        doctores.add(Doctores("234567" , "Sara Beltran"))
-        doctores.add(Doctores("345678" , "Jose Avila"))
+       // doctores.add(Doctores("123456" , "Diana Rios"))
+       // doctores.add(Doctores("234567" , "Sara Beltran"))
+       // doctores.add(Doctores("345678" , "Jose Avila"))
 
+        db = this.application as consultorioplanner
 
-
-        val adaptador = AdaptadorDoctor(this, doctores)
-        recycler.adapter = adaptador
+       // val adaptador = AdaptadorDoctor(this, doctores)
+     //   recycler.adapter = adaptador
     }
 
-    override fun onPostResume() {
-        super.onPostResume()
 
-    }
-
-    fun AgregarDoctor(v: View){
+    fun abrirAgregarDoctor(v: View){
         val intent= Intent(this,AgregarDoctor::class.java)
         startActivity(intent)
     }
-    fun abrirMostrar(v: View){
+    fun abrirMostrarCitas(v: View){
         val intent = Intent(this, MostrarCitas::class.java)
         startActivity(intent)
     }
+    override fun onPostResume() {
+        super.onPostResume()
+        actualizarRecycler()
+    }
+    fun actualizarRecycler(){
+        lifecycleScope.launch{
+            val lista= db.room.DoctoresDao().getAll()
+            actualizarRecyclerDespues(lista)
+        }
+    }
+    fun actualizarRecyclerDespues(list: List<Doctores>){
+        val adaptador = AdaptadorDoctor(this, list)
+        recycler.adapter = adaptador
+    }
+
+
 
 
 
